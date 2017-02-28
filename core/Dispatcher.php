b17db38c11dd74e9cd28cac9bcafdfc25bba3fea
Anas MORAHHIB <Anas MORAHHIB>
2017-02-28 23:21:34 +0000

    Layouts and auto render

--- core/Dispatcher.php
+++ core/Dispatcher.php
@@ -10,7 +10,19 @@
             $this->requeste = new Request();
             Route::decoup($this->requeste->url, $this->requeste);
             $controller = $this->loadController();
+            if(!in_array($this->requeste->action,get_class_methods($controller))){
+                $this->error('page not exist');
+            }
             call_user_func_array(array($controller,$this->requeste->action),$this->requeste->params);
+            $controller->render($this->requeste->action);
+        }
+
+        public function error($message){
+            header("HTTP/1.0 404 Not Found");
+            $controller = new Controller($this->requeste);
+            $controller->set('message',$message);
+            $controller->render('/errors/404');
+            die();
         }
 
         public function loadController(){
---
+++ view/layouts/default.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>Blog</title>
+        <link rel="stylesheet" href="<?= RACINE_URL ?>/style.css">
+        <link rel="stylesheet" href="<?= RACINE_URL ?>/bootstrap.css">
+    </head>
+
+    <body>
+        <div class="container">
+             <?= $content ?>
+        </div>
+
+    </body>
+
+</html>
